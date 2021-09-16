5f0377eddddd95392535b7d18bd3d26f62d5b17a
prateekKV <prateek.kerthivenkata@gmail.com>
2021-09-16 06:41:54 +0000

    3rd

--- backend/src/main/java/io/spring/RealworldApplication.java
+++ backend/src/main/java/io/spring/RealworldApplication.java
@@ -8,6 +8,6 @@ public class RealworldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RealworldApplication.class, args);
-		System.out.println("Hello from backend");
+		System.out.println("Hello");
 	}
 }
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
 import "./registerServiceWorker";
 import { CHECK_AUTH } from "./store/actions.type";
 import ApiService from "./common/api.service";
